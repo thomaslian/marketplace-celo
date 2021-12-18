8004d2a30871e41b52f68e579ecfbeeb69e71a5c
Thomas <thomas@notmyemail.com>
2021-12-18 22:36:02 +0100

    Added front-end and made it possible to interact with the contract

--- README.md
+++ README.md
@@ -5,8 +5,47 @@ Celo: [docs.celo.org/welcome](https://docs.celo.org/welcome)
 
 Build on Celo: [docs.celo.org/developer-guide/overview](https://docs.celo.org/developer-guide/overview)
 
-## Contract deployment
-Deployed at the Alfajores testnet with address [0x1963974600ba18C6127907e27D011eE71afD5165](https://alfajores-blockscout.celo-testnet.org/address/0x1963974600ba18C6127907e27D011eE71afD5165/transactions)
+## How to interact with this contract
 
-Transaction: [0x5cfe2aa09683e1fe8bd348460bcb86ae59d83fbbbe34db5849ee983aefeee7ce](https://alfajores-blockscout.celo-testnet.org/tx/0x5cfe2aa09683e1fe8bd348460bcb86ae59d83fbbbe34db5849ee983aefeee7ce/internal-transactions)
+Follow the steps below before you go to [website](https://celo-market.herokuapp.com/) to interact with the contract.
+
+1. Install the [CeloExtensionWallet](https://chrome.google.com/webstore/detail/celoextensionwallet/kkilomkmpmkbdnfelcpgckmpcaemjcdh?hl=en) from the Google Chrome Store.
+2. Create a wallet.
+3. Go to [celo.org/developers/faucet](https://celo.org/developers/faucet) and get some token for the alfajores testnet.
+4. Switch to the alfajores testnet in the CeloExtensionWallet.
+5. Try to add- or buy a product at the  [website](https://celo-market.herokuapp.com/).
+
+
+## Deployed Contract
+Deployed at the Alfajores testnet with contract address [0xb3b8F2f1ade9BEbd94433e8400000C5F7887606e](https://alfajores-blockscout.celo-testnet.org/address/0xb3b8F2f1ade9BEbd94433e8400000C5F7887606e/transactions)
+
+Transaction: [0x16e68aaeebc401fdd8b9b462036c86cc0616090e4df4dd7e2b241afbe7392dc2](https://alfajores-blockscout.celo-testnet.org/tx/0x16e68aaeebc401fdd8b9b462036c86cc0616090e4df4dd7e2b241afbe7392dc2/internal-transactions)
+
+## Run locally
+
+```
+
+npm install
+
+```
+
+```
+
+npm run dev
+
+```
+
+App should now be running at [http://localhost:3000/](http://localhost:3000/).
+
+
+## Build
+
+```
+
+npm run build
+
+```
+
+
+
 
---
+++ src/main.js
@@ -0,0 +1,200 @@
+import Web3 from 'web3';
+import { newKitFromWeb3 } from '@celo/contractkit';
+import BigNumber from "bignumber.js";
+import marketplaceAbi from '../contract/marketplace.abi.json';
+import erc20Abi from "../contract/erc20.abi.json";
+
+const MPContractAddress = "0xb3b8F2f1ade9BEbd94433e8400000C5F7887606e"; // Marketplace contract address
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"; // cUSD contract address
+const ERC20_DECIMALS = 18;
+
+let kit;
+let contract;
+let products = [];
+
+const connectCeloWallet = async () => {
+    if (window.celo) { // Check if Celo wallet is installed
+        notification("⚠️ Please approve this DApp to use it.");
+        try {
+            await window.celo.enable(); // Approve DApp to use Celo wallet
+            notificationOff();
+
+            const web3 = new Web3(window.celo); // Creates a web3 object using Celo as the provider
+            kit = newKitFromWeb3(web3); // Kit can now interact with the Celo Blockchain
+
+            const accounts = await kit.web3.eth.getAccounts(); // Get the accounts from the Celo wallet
+            kit.defaultAccount = accounts[0]; // Set the default account to the first account
+
+            contract = new kit.web3.eth.Contract(marketplaceAbi, MPContractAddress); // Create a contract object using the marketplace contract
+
+        } catch (e) {
+            notification(`⚠️ ${error}.`);
+        }
+    } else {
+        notification("⚠️ Please install the Celo Wallet extension.");
+    }
+}
+
+const approve = async (price) => {
+    const cUSDContract = new kit.web3.eth.Contract(erc20Abi, cUSDContractAddress); // Create a contract object using the cUSD contract
+
+    const result = await cUSDContract.methods
+        .approve(MPContractAddress, price)
+        .send({ from: kit.defaultAccount });
+    return result;
+}
+
+const getBalance = async () => {
+    const totalBalance = await kit.getTotalBalance(kit.defaultAccount); // Get the total balance of the default account
+    const cUSDBalance = totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2); // Get a readable cUSD balance
+    document.querySelector("#balance").textContent = cUSDBalance;
+}
+
+const getProducts = async () => {
+    const productsLength = await contract.methods.getProductsLength().call();
+    const _products = [];
+
+    for (let i = 0; i < productsLength; i++) {
+        let product = new Promise(async (resolve, reject) => {
+            let p = await contract.methods.readProduct(i).call(); // Use the getProduct method in the contract
+            resolve({ // Resolve the promise to return product data
+                index: i,
+                owner: p[0],
+                name: p[1],
+                image: p[2],
+                description: p[3],
+                location: p[4],
+                price: new BigNumber(p[5]), // Needs to be a bigNumber object 
+                sold: p[6],
+            });
+            reject(error => { console.log(error) }); // Reject the promise if there is an error    
+        });
+        _products.push(product);
+    }
+    products = await Promise.all(_products);
+    renderProducts();
+}
+
+function renderProducts() {
+    document.getElementById("marketplace").innerHTML = "";
+    products.forEach(product => {
+        const newDiv = document.createElement("div");
+        newDiv.className = "col-md-4";
+        newDiv.innerHTML = productTemplate(product);
+        document.getElementById("marketplace").appendChild(newDiv);
+    });
+}
+
+function productTemplate(product) {
+    return `
+        <div class="card mb-4">
+            <img class="card-img-top" src="${product.image}" alt="Card image">
+            <div class="position-absolute top-0 end-0 bg-warning mt-4 px-2 py-1 rounded-start">
+                ${product.sold} Sold
+            </div>
+            <div class="card-body text-left p-4 position-relative">
+            <div class="translate-middle-y position-absolute top-0">
+                ${identiconTemplate(product.owner)}
+            </div>
+            <h2 class="card-title fs-4 fw-bold mt-2">${product.name}</h2>
+            <p class="card-text mb-4" style="min-height: 82px">
+              ${product.description}             
+            </p>
+            <p class="card-text mt-4">
+              <i class="bi bi-geo-alt-fill"></i>
+              <span>${product.location}</span>
+            </p>
+            <div class="d-grid gap-2">
+              <a class="btn btn-lg btn-outline-dark buyBtn fs-6 p-3" id=${product.index}>
+                Buy for ${product.price.shiftedBy(-ERC20_DECIMALS).toFixed(2)} cUSD
+              </a>
+            </div>
+          </div>
+        </div>
+    `;
+}
+
+function identiconTemplate(address) {
+    const icon = blockies
+        .create({
+            seed: address,
+            size: 8,
+            scale: 16,
+        })
+        .toDataURL();
+
+    return `
+    <div class="rounded-circle overflow-hidden d-inline-block border border-white border-2 shadow-sm m-0">
+      <a href="https://alfajores-blockscout.celo-testnet.org/address/${address}/transactions"
+          target="_blank">
+          <img src="${icon}" width="48" alt="${address}">
+      </a>
+    </div>
+    `
+}
+
+function notification(_text) {
+    document.querySelector(".alert").style.display = "block";
+    document.querySelector("#notification").textContent = _text;
+}
+
+function notificationOff() {
+    document.querySelector(".alert").style.display = "none";
+}
+
+window.addEventListener("load", async () => {
+    notification("⌛ Loading...");
+    await connectCeloWallet();
+    await getBalance();
+    await getProducts();
+    notificationOff();
+});
+
+document.querySelector("#newProductBtn").addEventListener("click", async () => {
+    const params = [
+        document.getElementById("newProductName").value,
+        document.getElementById("newImgUrl").value,
+        document.getElementById("newProductDescription").value,
+        document.getElementById("newLocation").value,
+        // Create a bigNumber object so the contract can read it
+        new BigNumber(document.getElementById("newPrice").value).shiftedBy(ERC20_DECIMALS).toString()
+    ];
+    notification(`⌛ Adding "${params[0]}"...`);
+
+    try {
+        const result = await contract.methods
+            .writeProduct(...params)
+            .send({ from: kit.defaultAccount }) // Sebd a trabsaction to the contract, using the default account
+    } catch (error) {
+        notification(`⚠️ ${error}.`)
+    }
+    notification(`🎉 You successfully added "${params[0]}".`);
+    getProducts();
+});
+
+document.querySelector("#marketplace").addEventListener("click", async (e) => {
+    if (e.target.className.includes("buyBtn")) {
+        const index = e.target.id;
+
+        // Approve payment
+        notification("⌛ Waiting for payment approval...");
+        try {
+            await approve(products[index].price);
+        } catch (error) {
+            notification(`⚠️ ${error}.`);
+        }
+
+        // Do the payment
+        notification(`⌛ Awaiting payment for "${products[index].name}"...`);
+        try {
+            const result = await contract.methods
+                .buyProduct(index)
+                .send({ from: kit.defaultAccount });
+            notification(`🎉 You successfully bought "${products[index].name}".`);
+            getProducts();
+            getBalance();
+        } catch (error) {
+            notification(`⚠️ ${error}.`);
+        }
+    }
+});
