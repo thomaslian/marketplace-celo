50648721fd4ea5700263866b8fdd30530b0c9269
= <Emmanuelsumeh@gmail.com>
2021-12-24 11:34:51 +0100

    chore: updated MP contract address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-const MPContractAddress = "0x1Ddca354FF9269F9CC4122730A4B82F7eA66cb94"; // Marketplace contract address
+// const MPContractAddress = "0x1Ddca354FF9269F9CC4122730A4B82F7eA66cb94"; // Marketplace contract address
+const MPContractAddress = "0xF5D5C0446C9966826345Aa5EBa89ce385f09fFdA";
 const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"; // cUSD contract address
 const ERC20_DECIMALS = 18;
 
